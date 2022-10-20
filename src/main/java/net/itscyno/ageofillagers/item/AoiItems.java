19de73ab18d0c818fa1be9bd9f3ea4e9a1fea375
CYNO-MC <businessforthesteve@gmail.com>
2022-10-20 13:50:36 -0400

    Raw Tin Texture Update & Raw Zinc Item

--- src/main/java/net/itscyno/ageofillagers/item/AoiItems.java
+++ src/main/java/net/itscyno/ageofillagers/item/AoiItems.java
@@ -12,6 +12,9 @@ public class AoiItems {
     public static final Item RAW_TIN = registerItem("raw_tin",
             new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
+    public static final Item RAW_ZINC = registerItem("raw_zinc",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
     private static Item registerItem (String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(AoiMain.MOD_ID, name), item);
     }
